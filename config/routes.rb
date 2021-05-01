1813c9f4829d2357d1de383ffbc3d5ca095840ae
GGibson <gibsonguillermoarturo@gmail.com>
2021-05-01 14:06:18 -0400

    Cambiando la ruta de la página One como página principal al cargar el proyecto

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #get 'pages/one' -- Cambiando la ruta de page/one a la página principal por defecto
+  get 'pages/two'
+
+  root 'pages#one' #-- Página incial de las vistas
 end
