44456f100c4a780570146a2c52988ded69000a74
GGibson <gibsonguillermoarturo@gmail.com>
2021-05-01 17:20:37 -0400

    agregando mejora de sintaxis a la rutas del archivo routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'pages#one' #-- Página incial de las vistas
+
+  get 'one', to: 'pages#one'
   #get 'pages/one' -- Cambiando la ruta de page/one a la página principal por defecto
   get 'pages/two'
   get 'pages/three' #-- Creando la ruta para aplicar una Vista-Controlador
-
-  root :to => 'pages#one' #-- Página incial de las vistas
+  
 end
