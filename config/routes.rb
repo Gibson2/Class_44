01abe39d21f4040de341b4959c53b4dba5e02a8e
GGibson <gibsonguillermoarturo@gmail.com>
2021-05-01 14:30:43 -0400

    Creando una nueva Vista-Controlador llamado Three, se creo el metodo dentro del controlador de Pages, se creo una nueva página three.html.erb y se agrego la ruta en el archivo de routes.rb. Se agrego titulo y parrafo en la página Three.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #get 'pages/one' -- Cambiando la ruta de page/one a la página principal por defecto
   get 'pages/two'
+  get 'pages/three' #-- Creando la ruta para aplicar una Vista-Controlador
 
   root 'pages#one' #-- Página incial de las vistas
 end
