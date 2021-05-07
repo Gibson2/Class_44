8092772c792f9344418884a7574a7bfbddb622b3
GGibson <gibsonguillermoarturo@gmail.com>
2021-05-07 17:03:59 -0400

    Agregando el modelo Game al proyecto

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_05_07_205430) do
+
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+    t.decimal "year"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
